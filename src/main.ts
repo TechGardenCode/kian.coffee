e0867e330a6258a7c396044436312a7b8570243c
Tech Garden <techgardencode@gmail.com>
2025-08-21 12:22:20 -0400

    feat: init posthog

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import posthog from 'posthog-js';
 
+posthog.init('phc_FkPsD9wHkfK8hPtxUlR2SZw3tJBaCkeZbvVzDxQONai', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'always',
+  defaults: '2025-05-24',
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
